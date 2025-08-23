9b039dc7afb1d8ba8d9ed78a3b34971209133cdb
leticiaikemeh <leticiaikemeh@gmail.com>
2025-08-23 20:26:32 +0100

    edit sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,31 +1,41 @@
-CREATE TABLE Books (
-    book_id INT Primary Key,
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
-    author_id INT FOREIGN KEY REFERENCES Authors(author_id)
-    price DOUBLE(8, 2),
-    publication_date DATE
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    PRIMARY KEY (book_id),
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE Authors (
-    author_id INT Primary Key,
-    customer_nam VARCHAR(215) NOT NULL
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT NOT NULL AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL,
+    PRIMARY KEY (author_id)
 );
 
-CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT NOT NULL AUTO_INCREMENT,
+    customer_name INT NOT NULL,
     Email VARCHAR(215) UNIQUE,
-    address TEXT
+    address TEXT,
+    PRIMARY KEY (customer_id)
 );
 
-CREATE TABLE Orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT FOREIGN KEY REFERENCES Customers(customer_id)
-    order_date DATE
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT NOT NULL AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    order_date DATE,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
-    orderdetailid  INT PRIMARY KEY,
-    order_id INT FOREIGN KEY REFERENCES  Orders(order_id),
-    book_id INT FOREIGN KEY REFERENCES  Books(book_id),
-    quantity DOUBLE
+    orderdetailid INT NOT NULL AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL, 
+    quantity DOUBLE NOT NULL,
+    PRIMARY KEY (orderdetailid),
+    FOREIGN KEY (order_id) REFERENCES  Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES  Books(book_id)
 );
